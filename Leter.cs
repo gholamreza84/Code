c044b3a5c0fc596c2187ca4249186daf836ca2ec
PC <PC@DESKTOP-LAHBPSQ>
2024-11-26 19:34:17 +0330

    Add project files.

---
+++ Leter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    internal class Leter : ICode
+    {
+        public string Code(string text, int value)
+        {
+            return new string(text.Select(c => char.IsLetter(c) ? NumChar(c, value) : c).ToArray());
+        }
+        private char NumChar(char c, int value)
+        {
+            char offset = char.IsUpper(c) ? 'A' : 'a';
+            return (char)((((c - offset) + value) % 26) + offset);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your sentence.");
+            string text = Console.ReadLine();  // کاربر جمله رو وارد میکنه
+
+            Console.WriteLine("Please Enter a number.");
+            int value = int.Parse(Console.ReadLine());  // یک عدد برای تغییر جمله وارد میکنه
+
+            Console.Clear();
+
+            ICode c1 = new Leter();  // یک نمونه از کلاس لتر ساختم با توجه به اینترفیس
+
+            string code = c1.Code(text, value);  // و اینجا جمله اصلی و عدد وارد شده رو میریزه داخل کلاس لتر
+            Console.WriteLine("New sentence : " + code);
+
+            // FiggleFonts.Standard
+
+            Console.WriteLine("Press the keyword to see the main sentence.");
+            Console.ReadKey();
+
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+        }
+    }
+}
